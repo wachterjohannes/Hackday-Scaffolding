0a1ca1e64bd4e0f61a2da86dc38f51f5cd5f5e64
Michael Zangerle <michael.zangerle@gmail.com>
2013-06-27 17:57:06 +0200

    generic list template, task controller, added route

--- src/Hackday/ScaffoldBundle/Controller/TaskController.php
+++ src/Hackday/ScaffoldBundle/Controller/TaskController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class CategoryController extends ScaffoldController
+class TaskController extends ScaffoldController
 {
     /**
      * @return string
      */
     protected function getEntityName()
     {
-        return "HackdayScaffoldBundle:Category";
+        return"HackdayScaffoldBundle:Task";
     }
-
 }
